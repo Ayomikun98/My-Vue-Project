844a9e0b73fa7527b7bd8a669d92bc8c3d8ecb70
Ayomikun98 <jamesayantunji98@gmail.com>
2024-06-05 16:13:07 +0100

    Tried using Vue

---
+++ src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<script setup>
+import gitHubIcon from './icons/gitHubIcon.vue'
+</script>
+
+<template>
+  <nav>
+    <div class="navbar">
+      <gitHubIcon class="github-icon" />
+      <h1>Ayomikun98 GitHub.</h1>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+.navbar {
+  display: flex;
+  align-items: center;
+  gap: 10px;
+  justify-content: center;
+  background-color: orange;
+  /* background-color: #1B9AAA; */
+  text-align: center;
+  padding: 20px;
+  margin-bottom: 100px;
+}
+
+.github-icon {
+  font-size: 3rem;
+}
+</style>
